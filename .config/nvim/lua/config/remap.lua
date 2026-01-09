0be65cee03dd83b899b2dcacd0a05c8ec35bee2f
Camden McGath <camdenlmcgath@gmail.com>
2026-01-08 20:32:52 -0700

    added fzf-lua maps

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -7,3 +7,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>ff", require('fzf-lua').files)
+vim.keymap.set("n", "<leader>fb", require('fzf-lua').buffers)
+vim.keymap.set("n", "<leader>fq", require('fzf-lua').quickfix)
+vim.keymap.set("n", "<leader>fs", require('fzf-lua').treesitter)
--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.tabstop = 4
